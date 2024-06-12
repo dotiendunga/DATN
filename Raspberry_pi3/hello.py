3e580cb7ed3353c203fef6f2cd963df0049f8c41
dotiendunga <dungdo3171@gmail.com>
2024-06-13 00:20:49 +0700

    heheh

--- Raspberry_pi3/hello.py
+++ Raspberry_pi3/hello.py
@@ -1,14 +1,14 @@
-from serial import Serial
+import serial
+import pynmea2
 
-# Kết nối với mô-đul GPS qua cổng serial UART
-ser = Serial("/dev/ttyS0", 9600, timeout=1)
+def parse_gps(line):
+    msg = pynmea2.parse(line)
+    print((msg.timestamp, msg.latitude, msg.longitude))
 
-def read_gps_data():
-    while True:
-        # Đọc dữ liệu từ mô-đul GPS
-        gps_data = ser.readline().decode().strip()
-        # In dữ liệu ra terminal
-        print("GPS data:", gps_data)
 
-# Bắt đầu đọc dữ liệu GPS
-read_gps_data()
+serial_port = serial.Serial('/dev/ttyAMA0', 9600, timeout=5)
+while True:
+    line = serial_port.readline().decode('unicode_escape')
+
+    if 'GGA' in line:
+        parse_gps(line)

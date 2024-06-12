9dba4b4b0a328b75f7de07be791a17351a0b2682
dotiendunga <dungdo3171@gmail.com>
2024-06-13 00:59:48 +0700

    heheh

--- Raspberry_pi3/hello.py
+++ Raspberry_pi3/hello.py
@@ -1,45 +1,27 @@
+
 import serial
 import pynmea2
 
 # Mở cổng serial UART
-ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+ser = serial.Serial('/dev/serial0', 9600, timeout=1)
 
 while True:
     try:
         # Đọc dữ liệu từ cổng serial UART
-        data = ser.readline()
-        
-        # Giải mã dữ liệu từ cổng Serial UART với encoding ASCII
-        data_decoded = data.decode('ascii', errors='ignore')
-        
-        # In dữ liệu để kiểm tra
-        print(data_decoded)
+        data = ser.readline().decode('utf-8', errors='ignore')
         
         # Kiểm tra xem dữ liệu có phải là chuỗi NMEA không
-        if data_decoded.startswith('$GPGGA'):
+        if data.startswith('$GPGGA'):
             try:
                 # Phân tích chuỗi NMEA
-                msg = pynmea2.parse(data_decoded)
-                print(f'GPGGA: Latitude: {msg.latitude}, Longitude: {msg.longitude}')
+                msg = pynmea2.parse(data)
+                
+                # Trích xuất và in kinh độ, vĩ độ
+                latitude = msg.latitude
+                longitude = msg.longitude
+                print(f'Latitude: {latitude}, Longitude: {longitude}')
             except pynmea2.ParseError as e:
-                print("Lỗi parsing GPGGA:", e)
-        
-        elif data_decoded.startswith('$GPRMC'):
-            try:
-                # Phân tích chuỗi NMEA
-                msg = pynmea2.parse(data_decoded)
-                print(f'GPRMC: Speed (knots): {msg.spd_over_grnd}')
-            except pynmea2.ParseError as e:
-                print("Lỗi parsing GPRMC:", e)
-        
-        elif data_decoded.startswith('$GPVTG'):
-            try:
-                # Phân tích chuỗi NMEA
-                msg = pynmea2.parse(data_decoded)
-                print(f'GPVTG: Speed (km/h): {msg.spd_over_grnd_kmph}')
-            except pynmea2.ParseError as e:
-                print("Lỗi parsing GPVTG:", e)
-
+                print("Lỗi parsing:", e)
     except UnicodeDecodeError:
         print("Lỗi decoding: Không thể giải mã dữ liệu")
     except Exception as e:
